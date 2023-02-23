a532bf72e22dad2a36517ad2b093077858368153
Robert Santos <robertsantos.dfw@gmail.com>
2023-02-22 22:53:27 -0600

    uncommenting routes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/login", (req, res) => {
 });
 
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
